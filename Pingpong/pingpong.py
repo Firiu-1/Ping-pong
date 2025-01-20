88f7d2f8dbdd12665d3528ac5e926a79731b7eee
Firiu-1 <overlordb498@gmail.com>
2025-01-20 17:00:33 +0300

    Add files via upload

--- Pingpong/pingpong.py
+++ Pingpong/pingpong.py
@@ -2,8 +2,9 @@ from pygame import *
 from random import randint
 #подгружаем отдельно функции для работы со шрифтом
 #нам нужны такие картинки:
-img_back = "player1.png" #фон игры
-img_hero = "player1.png" #герой
+img_ball = "ball.png" #фон игры
+img_player = "player1.png" #герой
+img_player2 = "player2.png" #герой
 img_back = "back.jpg" #враг
 class GameSprite(sprite.Sprite):
  #конструктор класса
@@ -19,22 +20,48 @@ class GameSprite(sprite.Sprite):
        self.rect.y = player_y
    def reset(self):
        window.blit(self.image, (self.rect.x, self.rect.y))
+class GameSprite2(sprite.Sprite):
+ #конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed_y, player_speed_x):
+       #вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+       #каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed_y = player_speed_y
+       self.speed_x = player_speed_x
+       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
 class Player(GameSprite):
    #метод для управления спрайтом стрелками клавиатуры
    def update(self):
        keys = key.get_pressed()
-       if keys[K_UP] and self.rect.x > 5:
-           self.rect.x -= self.speed
-       if keys[K_DOWN] and self.rect.x < win_width - 80:
-           self.rect.x += self.speed
+       if keys[K_UP] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_DOWN] and self.rect.y < 400:
+           self.rect.y += self.speed
+
 class Player2(GameSprite):
    #метод для управления спрайтом стрелками клавиатуры
    def update(self):
        keys = key.get_pressed()
-       if keys[K_LEFT] and self.rect.x > 5:
-           self.rect.x -= self.speed
-       if keys[K_RIGHT] and self.rect.x < win_width - 80:
-           self.rect.x += self.speed
+       if keys[K_w] and self.rect.y > 5:
+           self.rect.y -= self.speed
+       if keys[K_s] and self.rect.y < 400:
+           self.rect.y += self.speed
+class ball(GameSprite2):
+    def update(self):
+        # Отскок от верхней и нижней границ
+        if self.rect.y <= 0 or self.rect.y >= win_height - self.rect.height:
+            self.speed_y *= -1
+        # Движение по координатам
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        if self.rect.colliderect(Player.rect) or self.rect.colliderect(Player2.rect):
+            self.speed_x *= -1
 #класс спрайта-врага  
 win_width = 700
 win_height = 500
@@ -42,8 +69,9 @@ display.set_caption("Шутер")
 window = display.set_mode((win_width, win_height))
 background = transform.scale(image.load(img_back), (win_width, win_height))
 #создаём спрайты
-player1 = Player(img_hero, 5, win_height - 100, 80, 100, 10)
-player2 = Player(img_hero, 5, 200 - 200, 200, 200, 10)
+player1 = Player(img_player, 5, 240 - 100, 15, 100, 0.7) #self, player_image, player_x, player_y, size_x, size_y, player_speed
+player2 = Player2(img_player2, 680, 240 - 100, 15, 100, 0.7)
+ball1 = ball(img_ball, 450, 250, 30, 30, 1, 1)
 #создание группы спрайтов-врагов
 #переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
 finish = False
@@ -54,19 +82,17 @@ while run:
    for e in event.get():
        if e.type == QUIT:
            run = False
-       #событие нажатия на пробел - спрайт стреляет
-       elif e.type == KEYDOWN :
-           if e.key == K_SPACE:
-               player1.fire()
- #сама игра: действия спрайтов, проверка правил игры, перерисовка
    if not finish:
        #обновляем фон
        window.blit(background,(0,0))
        #производим движения спрайтов
        player1.update()
        player2.update()
+       ball1.update()
        #обновляем их в новом местоположении при каждой итерации цикла
        player1.reset()
+       player2.reset()
+       ball1.reset()
     
        #проверка столкновения пули и монстров (и монстр, и пуля при касании исчезают))
 #       #TODO://возможный проигрыш: пропустили слишком много или герой столкнулся с врагом
